47d1a6bc896debba365874d80daa5f3a918c4ffe
ernest <shiwb@shideMacBook-Pro.local>
2024-08-26 18:56:49 +0800

    hunter: update contracts

--- call_contract.sh
+++ call_contract.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
 # 从文件中读取 JSON 数据
-json=$(cat ~/workspace/initia-2/Tucana/potv/deployed-addresses.json)
+json=$(cat /root/Tucana/potv/deployed-addresses.json)
 
 # 提取变量并赋值到局部变量中
 Config=$(echo $json | jq -r '.Config')
 PriceFeed=$(echo $json | jq -r '.PriceFeed')
 ChainContract=$(echo $json | jq -r '.ChainContract')
 Lend=$(echo $json | jq -r '.Lend')
+Collateral_1=$(echo $json | jq -r '.Collateral_1')
+Collateral_2=$(echo $json | jq -r '.Collateral_2')
 
 # 输出变量的值
 echo "Config: $Config"
 echo "PriceFeed: $PriceFeed"
 echo "ChainContract: $ChainContract"
 echo "Lend: $Lend"
+echo "Collateral_1: $Collateral_1"
+echo "Collateral_2: $Collateral_2"
+
+
+cast send $PriceFeed "setTokenPrices(address[],uint256[])" \
+  "[$Collateral_1, $Collateral_2]" \
+  "[1000000000000000000, 2000000000000000000]" \
+  --private-key f08a80d51fd9f9fa6cdbe450af4d2aa98a5d2632ab4847cfeac77233c911f832
+
+
+cast call $PriceFeed "owner()(address)"
+
+
 
 ### cast demo: query contract
 cast call 0x1234567890abcdef1234567890abcdef12345678 "getValue()"
--- deploy_contract.sh
+++ deploy_contract.sh
@@ -20,7 +20,15 @@ transfer_value(){
 transfer_value
 
 rm -rf artifacts cache .openzeppelin
-#PRIVATE_KEY=f08a80d51fd9f9fa6cdbe450af4d2aa98a5d2632ab4847cfeac77233c911f832 npx hardhat compile
+
 
 PRIVATE_KEY=f08a80d51fd9f9fa6cdbe450af4d2aa98a5d2632ab4847cfeac77233c911f832 \
 npx hardhat run scripts/deploy.ts  --network localhost
+
+
+PRIVATE_KEY=f08a80d51fd9f9fa6cdbe450af4d2aa98a5d2632ab4847cfeac77233c911f832 \
+npx hardhat run scripts/configSet.ts  --network localhost
+
+
+PRIVATE_KEY=f08a80d51fd9f9fa6cdbe450af4d2aa98a5d2632ab4847cfeac77233c911f832 \
+npx hardhat run scripts/supply.ts  --network localhost
